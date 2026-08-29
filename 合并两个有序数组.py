314f8bd1599f10d0c16fd854ff5e24e432bd0c53
Charlie <Charlie_ge2023@163.com>
2026-08-29 15:24:06 +0800

    合并两个有序数组，排序与双指针方法

--- 合并两个有序数组.py
+++ 合并两个有序数组.py
@@ -1,6 +1,25 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        
+        nums1[m:]=nums2
+        nums1.sort()
+        return nums1
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        sort=[]
+        p1,p2=0,0
+        while p1 < m or p2 < n:
+            if p1 == m:
+                sort.append(nums2[p2])
+                p2+=1
+            elif p2 == n:
+                sort.append(nums1[p1])
+                p1+=1
+            elif nums1[p1]<nums2[p2]:
+                sort.append(nums1[p1])
+                p1+=1
+            else:
+                sort.append(nums2[p2])
+                p2+=1
+        nums1[:]=sort
+        return nums1
+
