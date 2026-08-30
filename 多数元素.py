a648a1f84670c905dda62c7fbd514e098cb154ac
Charlie <Charlie_ge2023@163.com>
2026-08-30 14:44:21 +0800

    投票法

--- 多数元素.py
+++ 多数元素.py
@@ -9,3 +9,12 @@ class Solution:
             else:hashtable[j]=1
             if hashtable[j]>len(nums)/2:
                 return j
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        candidate=None
+        count=0
+        for num in nums:
+            if count==0:
+                candidate=num
+            count+=1 if nums==candidate else count-=1
+        return candidate
