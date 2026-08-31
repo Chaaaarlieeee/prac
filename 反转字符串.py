46427a9737dc793c145fbd45f9e623b90bf97e93
Charlie <Charlie_ge2023@163.com>
2026-08-31 09:11:21 +0800

    反转字符串，双指针

---
+++ 反转字符串.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        if len(s)==1:
+            return s
+        l=0
+        r=len(s)-1
+        while l<r:
+            temp=s[l]
+            s[l]=s[r]
+            s[r]=temp
+            l+=1
+            r-=1
